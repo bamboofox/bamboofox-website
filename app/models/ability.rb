468a3dbd73fb5b98f3cd0d594e9f7b173671cff6
bananaappletw <bananaappletw@gmail.com>
2017-07-06 16:08:33 +0800

    Add ability for challenge and material

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,8 @@ class Ability
     user ||= User.new
     can :manage, user
     can :read, Course
+    can %i[read submit], Challenge
+    can :read, Material
     can :manage, :all if user.has_role? :admin
   end
 end
