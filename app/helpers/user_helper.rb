e9253ab5974ad3d75d3149fbcdc4962c56b6f75b
bananaappletw <bananaappletw@gmail.com>
2017-07-06 15:03:51 +0800

    Add default avatar

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,17 @@
 module UserHelper
   def icon_avatar(user, options = {})
-    image_tag user.avatar.icon.url, options
+    if user.avatar?
+      image_tag user.avatar.icon.url, options
+    else
+      image_tag 'icon_avatar.jpg', options
+    end
   end
 
   def profile_avatar(user, options = {})
-    image_tag user.avatar.profile.url, options
+    if user.avatar?
+      image_tag user.avatar.profile.url, options
+    else
+      image_tag 'profile_avatar.jpg', options
+    end
   end
 end
