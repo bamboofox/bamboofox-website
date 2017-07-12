a2706a4f81a035713ea038a1c0cffcd4cb85043d
bananaappletw <bananaappletw@gmail.com>
2017-07-12 23:58:38 +0800

    fix syntax

--- db/schema.rb
+++ db/schema.rb
@@ -10,96 +10,94 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712145211) do
-
-  create_table "challenge_submissions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "course_id"
-    t.integer "user_id"
-    t.integer "challenge_id"
-    t.index ["challenge_id"], name: "index_challenge_submissions_on_challenge_id"
-    t.index ["course_id"], name: "index_challenge_submissions_on_course_id"
-    t.index ["user_id"], name: "index_challenge_submissions_on_user_id"
+ActiveRecord::Schema.define(version: 20_170_712_145_211) do
+  create_table 'challenge_submissions', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'course_id'
+    t.integer 'user_id'
+    t.integer 'challenge_id'
+    t.index ['challenge_id'], name: 'index_challenge_submissions_on_challenge_id'
+    t.index ['course_id'], name: 'index_challenge_submissions_on_course_id'
+    t.index ['user_id'], name: 'index_challenge_submissions_on_user_id'
   end
 
-  create_table "challenges", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "flag"
-    t.integer "point"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "course_id"
-    t.string "attachments"
-    t.text "hint"
-    t.index ["course_id"], name: "index_challenges_on_course_id"
+  create_table 'challenges', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.string 'flag'
+    t.integer 'point'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'course_id'
+    t.string 'attachments'
+    t.text 'hint'
+    t.index ['course_id'], name: 'index_challenges_on_course_id'
   end
 
-  create_table "courses", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'courses', force: :cascade do |t|
+    t.text 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "identities", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "provider"
-    t.string "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_identities_on_user_id"
+  create_table 'identities', force: :cascade do |t|
+    t.integer 'user_id'
+    t.string 'provider'
+    t.string 'uid'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_identities_on_user_id'
   end
 
-  create_table "materials", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "attachment"
-    t.integer "course_id"
-    t.index ["course_id"], name: "index_materials_on_course_id"
+  create_table 'materials', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'attachment'
+    t.integer 'course_id'
+    t.index ['course_id'], name: 'index_materials_on_course_id'
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["name"], name: "index_roles_on_name"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name'
+    t.string 'resource_type'
+    t.integer 'resource_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
+    t.index ['name'], name: 'index_roles_on_name'
+    t.index %w[resource_type resource_id], name: 'index_roles_on_resource_type_and_resource_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "avatar"
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'avatar'
+    t.string 'name'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  create_table 'users_roles', id: false, force: :cascade do |t|
+    t.integer 'user_id'
+    t.integer 'role_id'
+    t.index ['role_id'], name: 'index_users_roles_on_role_id'
+    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
+    t.index ['user_id'], name: 'index_users_roles_on_user_id'
   end
-
 end
