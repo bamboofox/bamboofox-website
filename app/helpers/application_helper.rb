b7363a02bb6a6a86decf11727b997c0bd9390b59
bananaappletw <bananaappletw@gmail.com>
2017-07-12 23:30:42 +0800

    Add markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true,
+                                  hard_wrap: true)
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      strikethrough: true,
+      lax_html_blocks: true,
+      superscript: true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    sanitize markdown_to_html.render(text)
+  end
 end
