7ac61f07e6bd7ae5acc50973b3022dfd9ee986c6
bananaappletw <bananaappletw@gmail.com>
2017-07-21 18:51:41 +0800

    Add kaminari pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
 
-  def index; end
+  def index
+    @users = User.accessible_by(current_ability, :index).page params[:page]
+  end
 
   def show; end
 end
