8ceb57320441fd1c4a58e82aa583133cf548b6da
bananaappletw <bananaappletw@gmail.com>
2017-07-26 20:48:06 +0800

    Add user management

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,48 @@
+class Admin::UsersController < AdminController
+  load_and_authorize_resource
+
+  def index
+    @users = User.accessible_by(current_ability, :index).page params[:page]
+  end
+
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user), notice: 'User was successfully created!'
+    else
+      redirect_to new_admin_user_path, alert: 'Some fields are empty or not valid...'
+    end
+  end
+
+  def update
+    params[:user][:role_ids] ||= []
+    if params[:user][:password].present?
+      if @user.update_attributes(user_params)
+        redirect_to admin_user_path(@user), notice: 'User was successfully updated!'
+      else
+        render :edit
+      end
+    elsif @user.update_without_password(user_params)
+      redirect_to admin_user_path(@user), notice: 'User was successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, alert: 'User was successfully deleted!'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :avatar_cache, { role_ids: [] }, :confirmed_at)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,9 @@ Rails.application.routes.draw do
     # About
     resource :about, only: %i[show edit update]
 
+    # User
+    resources :users
+
     # Rank
     resource :rank, only: [:show]
 
