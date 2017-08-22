625c96aadd33428673e9c202fc77a47dcbf319f1
bananaappletw <bananaappletw@gmail.com>
2017-08-22 16:31:55 +0800

    Upgrade omniauth-nctu

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,15 +30,9 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     joins(:identities).where(identities: { provider: auth.provider, uid: auth.uid }).first_or_initialize do |user|
-      case auth.provider
-      when 'nctu'
-        user.name = auth.extra.raw_info.username
-        user.email = auth.extra.raw_info.email
-      else
-        user.name = auth.info.name
-        user.email = auth.info.email
-        user.remote_avatar_url = auth.info.image
-      end
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.remote_avatar_url = auth.info.image
       user.password = Devise.friendly_token[0, 20]
       user.identities.build(provider: auth.provider, uid: auth.uid)
       user.skip_confirmation!
