75fe913c7fcf0b356ccbb49bc34e665f6b6bdf30
bananaappletw <bananaappletw@gmail.com>
2019-06-29 14:33:00 +0800

    Add is_email_visible to sign up form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,7 +40,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar avatar_cache name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[is_email_visible name avatar avatar_cache])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
